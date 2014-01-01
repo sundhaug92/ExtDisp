66362cb20e23cec899d1f74f97409b0ad1df81a1
Martin Sundhaug <martinsundhaug@gmail.com>
2014-01-01 14:36:12 +0100

    Imported existing code-base

---
+++ ExtDisp/ExtDisp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+
+namespace ExtDisp
+{
+    public class ExtDisp : KSP.Testing.UnitTest
+    {
+        public ExtDisp()
+        {
+            GameObject ghost=new GameObject("ExtDispGhost",typeof(ExtDispGhost));
+            GameObject.DontDestroyOnLoad(ghost);
+        }
+    }
+    public class ExtDispGhost:MonoBehaviour
+    {
+        public static void WriteDebug(object obj) { print("ExtDispGhost: "+obj); }
+        WebServer ws;
+        public void Awake()
+        {
+            ws = new WebServer();
+            ws.Listen(1992);
+        }
+        
+        public void Update()
+        {
+            if (FlightGlobals.ready && FlightGlobals.ActiveVessel != null)
+            {
+                Vessel vessel = FlightGlobals.ActiveVessel;
+
+                StringBuilder sb = new StringBuilder();
+
+                sb.Append("{");
+                sb.AppendFormat("mainThrottle:{0},", vessel.ctrlState.mainThrottle);
+                sb.AppendFormat("X:{0},", vessel.ctrlState.X);
+                sb.AppendFormat("Y:{0},", vessel.ctrlState.Y);
+                sb.AppendFormat("Z:{0},", vessel.ctrlState.Z);
+                sb.AppendFormat("ApA:{0},", vessel.orbit.ApA);
+                sb.AppendFormat("PeA:{0},", vessel.orbit.PeA);
+                sb.AppendFormat("Vel:{0}", vessel.orbit.vel);
+                sb.Append("}");
+
+                ws.Broadcast(sb.ToString());
+                //print(sb);
+            }
+            else print("Ready: " + FlightGlobals.ready + " & ActiveVessel:" + FlightGlobals.ActiveVessel);
+        }
+    }
+}
---
+++ ExtDisp/WebServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace ExtDisp
+{
+    class WebServer
+    {
+        TcpListener tl;
+        internal void Listen(int p)
+        {
+            tl = new TcpListener(new IPAddress(new byte[]{0,0,0,0}),p);
+            tl.Start();
+            Thread t = new Thread(worker);
+            t.Start();
+        }
+        public void worker()
+        {
+            while (true)
+            {
+                new Thread(clientHandler).Start(tl.AcceptTcpClient());
+            }
+        }
+        public void clientHandler(object _tc)
+        {
+            TcpClient tc = (TcpClient)_tc;
+            NetworkStream ns = new NetworkStream(tc.Client);
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+
+            StreamWriters.Add(sw);
+        }
+        List<StreamWriter> StreamWriters = new List<StreamWriter>();
+        internal void Broadcast(string p)
+        {
+            foreach (StreamWriter sw in StreamWriters)
+            {
+                sw.WriteLine(p);
+            }
+        }
+    }
+}
